afabb91cdd0174617b5ca156555f8092be94ae02
Emmanuel V. <51292782+emmanuelvelmo@users.noreply.github.com>
2025-05-24 13:45:41 -0700

    Multiple text file reader script.cpp

--- Multiple text file reader script.cpp
+++ Multiple text file reader script.cpp
@@ -1,1 +1,41 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <filesystem>
 
+int main()
+{
+    // Ruta del directorio de entrada
+    std::string ruta_dir;
+
+    std::cout << "Enter directory: ";
+    std::getline(std::cin, ruta_dir);
+
+    std::cout << "\n------------------------------------\n";
+
+    // Recorrer todos los archivos en la carpeta
+    for (const std::filesystem::directory_entry &entrada : std::filesystem::directory_iterator(ruta_dir))
+    {
+        // Verifica si es un archivo regular
+        if (entrada.is_regular_file())
+        {
+            std::ifstream archivo(entrada.path());
+
+            // Verifica si se puede abrir el archivo
+            if (archivo)
+            {
+                std::string contenido_val((std::istreambuf_iterator<char>(archivo)), std::istreambuf_iterator<char>());
+
+                // Imprimir el nombre y el contenido del archivo
+                std::cout << entrada.path().filename().string() << "\n\n";
+                std::cout << contenido_val << "\n";
+                std::cout << "------------------------------------\n";
+            }
+        }
+    }
+
+    // Espera a que el usuario presione Enter antes de cerrar
+    std::cin.get();
+
+    return 0;
+}
