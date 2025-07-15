4e8299947864e5c5ffa3ce1fd51d63b97c82bf8a
Emmanuel V. <51292782+emmanuelvelmo@users.noreply.github.com>
2025-07-15 08:35:37 -0700

    Multiple text file reader script.py

--- Multiple text file reader script.py
+++ Multiple text file reader script.py
@@ -1,5 +1,8 @@
 import os
 
+# Lista de codificaciones a probar (en orden de prioridad)
+codificaciones_txt = ['utf-8', 'latin-1', 'utf-16', 'cp1252']
+
 ruta_dir = input("Enter directory: ")
 
 print("\n------------------------------------")
@@ -11,18 +14,22 @@ for nombre_archivo in os.listdir(ruta_dir):
     
     # Verifica si es un archivo (y no una carpeta)
     if os.path.isfile(ruta_completa):
-        try:
-            # Intenta abrir y leer el contenido del archivo como texto
-            with open(ruta_completa, 'r', encoding='utf-8') as archivo:
-                contenido_val = archivo.read()
-                
-                # Si no hay excepción, imprime el nombre y el contenido del archivo
-                print(f"{nombre_archivo}\n")
-                print(contenido_val)
-                print("------------------------------------")
-        except (UnicodeDecodeError, PermissionError, IOError):
-            # Si ocurre un error, simplemente continúa con el siguiente archivo
-            continue
+        # Intenta decodificar en cada formato
+        for codificacion_iter in codificaciones_txt:  
+            try:
+                # Intenta abrir y leer el contenido del archivo como texto
+                with open(ruta_completa, 'r', encoding = codificacion_iter) as archivo:
+                    contenido_val = archivo.read()
+                    
+                    # Si no hay excepción, imprime el nombre y el contenido del archivo
+                    print(f"{nombre_archivo}\n")
+                    print(contenido_val)
+                    print("------------------------------------")
+
+                    break
+            except (UnicodeDecodeError, PermissionError, IOError):
+                # Si ocurre un error, simplemente continúa con el siguiente archivo
+                continue
 
 # Espera a que el usuario presione Enter antes de cerrar
 input()
